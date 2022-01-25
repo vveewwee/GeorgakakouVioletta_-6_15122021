983126229c183f6adeedfe8d4697010f8eef9f69
Violetta GEORGAKAKOU <vgeorgak@e3r3p19.clusters.42paris.fr>
2022-01-25 16:51:43 +0100

    photographers pages

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -5,16 +5,39 @@ const getData = async () =>{
     return (data);
 };
 
-const displayMedia = (media) =>{
-    media.forEach((photographerMedia) => {
-            const photoDisplayDiv = document.querySelector(".photograph-display");
+const displayMedia = (photographer, mediaE) =>{
+    const photoDisplayArticle = document.createElement ('article');
+            photoDisplayArticle.setAttribute("class", "photograph-display");
+            const main = document.querySelector("#main");
+            main.appendChild(photoDisplayArticle);
+
+    mediaE.forEach((photographerMedia) => {
+            const photoDisplayDiv = document.createElement('div');
+            photoDisplayDiv.setAttribute("class", "photograph-div");
+            photoDisplayArticle.appendChild(photoDisplayDiv);
+            
             const photos = document.createElement('img');
-            photos.setAttribute("class", "photodisplay");
+            photos.setAttribute("class", "photograph-img");
             const imagePhoto = photographerMedia.image;
-            photos.setAttribute("src", imagePhoto);
+            photos.src ="SamplePhotos/"+ photographer.name +"/" + imagePhoto;
             photoDisplayDiv.append(photos);
-//                console.log(individualPhoto);
-//            photographerMediaFactory(individualPhoto);
+
+            const legende = document.createElement('div');
+            legende.setAttribute("class", "photographe-legende");
+            photoDisplayDiv.appendChild(legende);
+
+            const titlePhoto = document.createElement('h5');
+            titlePhoto.innerText = photographerMedia.title;
+            legende.appendChild(titlePhoto);
+
+            const likes = document.createElement('p');
+            likes.innerText = photographerMedia.likes;
+            legende.appendChild(likes);
+
+            const iconHeart = document.createElement('i');
+            iconHeart.classList.add('fas','fa-heart', 'heartIcon');
+            likes.appendChild(iconHeart);
+
     });
 };
 
@@ -25,5 +48,5 @@ window.onload = async() =>{
     const photographer = data.photographers.find(item => item.id == id);
     const mediaE = data.media.filter(item => item.photographerId == id);
 //    console.log(mediaE);
-    displayMedia(mediaE);
+    displayMedia(photographer, mediaE);
 };
